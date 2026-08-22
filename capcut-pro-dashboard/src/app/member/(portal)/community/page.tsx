dd88c5e445458db317e6908c136a227bd73111d9
ridoazimi <ridoazimi@gmail.com>
2026-08-22 18:56:52 +0700

    revert: remove Community notification gate

--- capcut-pro-dashboard/src/app/member/(portal)/community/page.tsx
+++ capcut-pro-dashboard/src/app/member/(portal)/community/page.tsx
@@ -1,10 +1,5 @@
 import CommunityClient from "./CommunityClient";
-import CommunityNotificationGate from "./CommunityNotificationGate";
 
 export default function MemberCommunityPage() {
-  return (
-    <CommunityNotificationGate>
-      <CommunityClient />
-    </CommunityNotificationGate>
-  );
+  return <CommunityClient />;
 }
